d51edd35ccebe3bb86ef8bcf6d18df756d1ffae2
briplomo1 <briplomo@gmail.com>
2021-11-07 03:26:10 -0600

    added exceptions

--- Schedule.py
+++ Schedule.py
@@ -1,27 +1,15 @@
 import time
 import sys
+from my_exceptions import NoAvailabilityException
 
 sys.setrecursionlimit(2000)
-
-schedule = [
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
-    [None, None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
-]
- # Constants- employees needed for each shift minimum and maximum
-# 4 shifts per day. Employees needed per shift
-MaxEmployeesPerShift = [2,2,2,1]
-NumberOfEmployees = len(schedule)
 
 employees = [
     {
         'id':1,
         'availability':[
             True, True, False, True,
-            False, False, False, False,
+            True, False, False, False,
             False, True, False, False,
             False, False, False, False,
             False, True, True, False,
@@ -29,12 +17,12 @@ employees = [
         ],
         'doubles': False,
         'lvn': False,
-        'max': 40
+        'max': 4
     },
     {
         'id':2,
         'availability':[
-            False, False, False, False,
+            True, False, False, False,
             False, False, True, True,
             False, True, False, False, 
             False, False, True, True,
@@ -48,7 +36,7 @@ employees = [
     {
         'id':3,
         'availability':[
-            True, True, True, False,
+            False, True, True, False,
             True, True, False, False,
             False, True, True, False,
             True, True, False, False,
@@ -62,7 +50,7 @@ employees = [
     {
         'id':4,
         'availability':[
-            True, False, True, True,
+            False, False, True, True,
             True, True, True, True,
             False, False, False, False,
             True, True, True, True,
@@ -105,7 +93,23 @@ employees = [
 ]
 
 
+schedule = []
+ # Constants- employees needed for each shift minimum and maximum
+# 4 shifts per day. Employees needed per shift
+MaxEmployeesPerShift = [2,2,2,1]
+NumberOfEmployees = len(employees)
+NumberOfShifts = 24
+
+
+
+
 ############################   HELPER FUNCTIONS #############################################
+def initiate_sched(emps, shifts, m):
+    for e in range(emps):
+        r = []
+        for i in range(shifts):
+            r.append(0)
+        m.append(r)
 
 def print_matrix():
     print("\n")
@@ -141,12 +145,29 @@ def get_col_hours(j, schedule):
             hours += schedule[e][j]
     return hours
 
-def has_availability(col):
+def no_availability(m):
     #print(f"Checking availability for col {col}")
-    for emp in employees:
-        if emp['availability'][col]:
-            return True
-    #print(f"Col {col} has no availability")
+    for col in range(len(m[0])):
+        for emp in range(len(employees)):
+            if employees[emp]['availability'][col]:
+                break
+            elif emp == len(employees)-1:
+                print(f"No availability for col {col}")
+                return col+1
+
+    return False
+
+def not_enough_staff():
+    for shift in range(NumberOfShifts):
+        needed = MaxEmployeesPerShift[shift%4]
+        scheduled = 0
+        for e in range(NumberOfEmployees):
+            if employees[e]['availability'][shift]:
+                scheduled += 1
+        print(scheduled)
+        if needed > scheduled:
+            # print(f"Not enough scheduled {shift}, {scheduled}")
+            return shift+1
     return False
 ############################################## MAIN FUNCTIONS ###########################################
 
@@ -183,7 +204,6 @@ def find_open_shift(m):
     return False
 
 def solve_schedule(m):
-
     #print("Finding open shift...")
     avail_col = find_open_shift(m)
     if not avail_col:
@@ -194,15 +214,6 @@ def solve_schedule(m):
         return True
     else:
         _, c = avail_col
-
-    if not has_availability(c):
-        print(f"\n\nERROR!!! -----------------  Could not find availability for column {c} !!!   -------------------\n\n   ::::::::::::::::::::: Ouputting incomplete schedule due to schedule conflict::::::::::::::::::::::")
-        reformat_matrix(m)
-   
-        print("  --------------- Scheduler must manually make changes to schedule OR change employee availability  -------------------------\n\n")
-        print("                       .....ENDING PROGRAM.....\n\n")
-        print("\nReturning false just cuz...\n")
-        return True
 
     for e in range(len(m)):
         
@@ -228,20 +239,32 @@ def solve_schedule(m):
             print_matrix()
 
     # if column is not full
-    if is_column_not_full(c,m):
+    if is_column_not_full(c,m) and c != 0:
         #print(f"Column {c} not filled! Backtracking...")
         # if it has not backtracked all the way to the first column it returns zero and backtracks
-        if c != 0:
-            return False
-        else:
-            #print("Could not resolve conflict...")
-            pass
+        return False
+
     return False
 
 
-reformat_matrix(schedule)
+def solve(m):
+    error = no_availability(m)
+    if not error:
+        error = not_enough_staff()
+    print(error)
+    if error > 0:
+        print_matrix()
+        raise NoAvailabilityException(error)
+        
+    else:
+        solve_schedule(m)
+
+
+
+initiate_sched(NumberOfEmployees, NumberOfShifts, schedule)
+# reformat_matrix(schedule)
 startT = time.time()
-solve_schedule(schedule)
+solve(schedule)
 
 print(time.time()-startT)
 
